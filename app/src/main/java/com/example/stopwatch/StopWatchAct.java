ade6ffbfd078e870868fd1cc0a24b8b99f29c72b
AngelikaJustine <fay.ariana@gmail.com>
2019-11-24 19:51:11 +0700

    Update Notification

--- app/src/main/java/com/example/stopwatch/StopWatchAct.java
+++ app/src/main/java/com/example/stopwatch/StopWatchAct.java
@@ -1,9 +1,14 @@
 package com.example.stopwatch;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.graphics.Typeface;
 import android.graphics.drawable.Animatable;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.SystemClock;
 import android.view.View;
@@ -20,10 +25,21 @@ public class StopWatchAct extends AppCompatActivity {
     Chronometer timerHere;
     Animation roundingalone;
 
+    private static final String CHANNEL_ID = "simplified_coding";
+    private static final String CHANNEL_NAME = "Simplified Ccoding";
+    private static final String CHANNEL_DESC = "Simplified Coding Notification";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stop_watch);
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, CHANNEL_NAME, NotificationManager.IMPORTANCE_DEFAULT);
+            channel.setDescription(CHANNEL_DESC);
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(channel);
+        }
 
         btnstart = findViewById(R.id.btnstart);
         btnstop = findViewById(R.id.btnstop);
@@ -48,6 +64,7 @@ public class StopWatchAct extends AppCompatActivity {
                 btnstart.animate().alpha(0).setDuration(300).start();
                 timerHere.setBase(SystemClock.elapsedRealtime());
                 timerHere.start();
+                displayNotification();
             }
         });
 
@@ -61,5 +78,18 @@ public class StopWatchAct extends AppCompatActivity {
             }
         });
 
+        
+
+    }
+
+    private void displayNotification(){
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(R.drawable.timer)
+                .setContentTitle("The Stopwatch Has Started")
+                .setContentText("Do your workout seriously")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+        NotificationManagerCompat mNotification = NotificationManagerCompat.from(this);
+        mNotification.notify(1, mBuilder.build());
     }
 }
